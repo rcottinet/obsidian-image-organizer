5928822229e6f57066466c5ade858e60dd265ef3
rcottinet <romancottinet@gmail.com>
2023-01-15 14:55:15 -0800

    feat: add feature to choose where file are saved

--- main.ts
+++ main.ts
@@ -1,81 +1,84 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, TFile, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+const DEFAULT_HANDLED_FILE_EXTENSION: Record<string, boolean> = {
+	'jpg': true,
+	'pdf': true,
+	'jpeg': true,
+	'png': true
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_TARGET_OPTIONS: Record<string, string> = {
+	'root': 'Root Folder',
+	'activeFolder': 'Active Folder'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+interface ImageOrganizerPluginSettings {
+	selectedTarget: string,
+	targetFolder: string;
+	targetOptions: Record<string, string>,
+	handledFileExtension: Record<string, boolean>,
+}
+
+const DEFAULT_SETTINGS: ImageOrganizerPluginSettings = {
+	selectedTarget: 'activeFolder',
+	targetOptions: DEFAULT_TARGET_OPTIONS,
+	targetFolder: 'media',
+	handledFileExtension: DEFAULT_HANDLED_FILE_EXTENSION
+}
+
+export default class ImageOrganizerPlugin extends Plugin {
+	settings: ImageOrganizerPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.registerEvent(app.vault.on('create', (file: TFile) => {
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+			/**
+			 * Check if the file extension is handle by the plugin 
+			 * and if extension setting is activated
+			 */
+			if (!Object.values(Object.keys(this.settings.handledFileExtension)).includes(file.extension)
+				|| !this.settings.handledFileExtension[file.extension]) {
+				return
+			}
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+			/**
+			 * Getting the active file to get the current path
+			 */
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+
+			let activeFile = app.workspace.getActiveFile();
+			if (!activeFile) {
+				return
 			}
-		});
+
+			let rootFolder = '';
+			let activeFolder = activeFile.parent.path || rootFolder;
+
+			let targetPath = this.settings.selectedTarget == 'activeFolder' ? activeFolder : rootFolder;
+
+			/**
+			 * Adding to the target path the folder specify or no in plugin setting
+			 */
+			if (this.settings.targetFolder.length > 0) {
+				targetPath += '/' + this.settings.targetFolder;
+			}
+
+			if (!app.vault.getAbstractFileByPath(targetPath)) {
+				app.vault.createFolder(targetPath).then(() => {
+					app.vault.rename(file, targetPath + '/' + file.name)
+						.then(() => new Notice("File successfully add to " + targetPath))
+				})
+			} else {
+				app.vault.rename(file, targetPath + '/' + file.name)
+					.then(() => new Notice("File successfully add to " + targetPath))
+			}
+		}));
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new ImageOrganizerPluginSettingTab(this.app, this));
 	}
 
 	onunload() {
@@ -91,47 +94,55 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class ImageOrganizerPluginSettingTab extends PluginSettingTab {
+	plugin: ImageOrganizerPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: ImageOrganizerPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', { text: 'Settings for Image Organizer Plugin' });
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+			.setName('Save media into')
+			.addDropdown(cb => cb
+				.addOptions(this.plugin.settings.targetOptions)
+				.setValue(this.plugin.settings.selectedTarget)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.selectedTarget = value;
 					await this.plugin.saveSettings();
 				}));
+
+		new Setting(containerEl)
+			.setName('Target folder')
+			.setDesc("Folder where file will be saved. \n Leave empty if you don't want to use a folder")
+			.addText(text => text
+				.setValue(this.plugin.settings.targetFolder)
+				.onChange(async (value) => {
+					this.plugin.settings.targetFolder = value;
+					await this.plugin.saveSettings();
+				}));
+
+		let extensionRecords = this.plugin.settings.handledFileExtension
+
+		containerEl.createEl('h2', { text: 'The plugin is activated for the following extension' });
+
+		for (const key in extensionRecords) {
+			const value = extensionRecords[key];
+			new Setting(containerEl)
+				.setName(key)
+				.addToggle(cb => cb
+					.setValue(value)
+					.onChange(async (newValue) => {
+						this.plugin.settings.handledFileExtension[key] = newValue;
+						await this.plugin.saveSettings();
+					}));
+		}
 	}
 }
